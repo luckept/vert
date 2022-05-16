0c3921e7d0272d09414ddb333ee1dd7c704f6af8
luckept <luckept@qq.com>
2022-05-16 14:51:18 +0800

    feat: 优化 createSourceEle 方法，为后续引入 CSS 模块提供支持

--- theme.js
+++ theme.js
@@ -10,9 +10,9 @@
      * 装配 TS 文件
      * @param {string} sourceName 资源路径
      */
-    static wiredTS(sourceName) {
+    static wiredTS(sourceName, type='script') {
       BASE_PATH = this.pathConfig.wiredTS
-      this.createSourceEle(sourceName, BASE_PATH)
+      this.createSourceEle(sourceName, BASE_PATH, type)
     }
 
     /**
@@ -20,11 +20,14 @@
      * @param {string} sourceName 资源名称
      * @param {string} BASE_PATH 资源根路径
      */
-    static createSourceEle(sourceName, BASE_PATH, scriptID='vert', RegExp=/[^.]*$/) {
-      sourceName = BASE_PATH + sourceName.replace(RegExp, 'js')
-      const source = document.createElement("script")
-      source.setAttribute("src", sourceName)
-      source.setAttribute("type", "module")
+    static createSourceEle(sourceName, BASE_PATH, type, scriptID='vert', RegExp=/[^.]*$/) {
+      let source
+      if (type === 'script') {
+        sourceName = BASE_PATH + sourceName.replace(RegExp, 'js')
+        source = document.createElement("script")
+        source.setAttribute("type", "module")
+        source.setAttribute("src", sourceName)
+      }
       source.setAttribute("id", scriptID)
       document.head.appendChild(source)
     }
